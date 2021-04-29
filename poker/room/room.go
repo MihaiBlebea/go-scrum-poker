87c47d6780b96e477612a27108acbc13de7f1e86
Mihai Blebea <mihai@getchip.uk>
2021-04-29 18:03:57 +0100

    Adds custom url for joining a room

--- poker/room/room.go
+++ poker/room/room.go
@@ -1,6 +1,7 @@
 package room
 
 import (
+	"math/rand"
 	"time"
 
 	"github.com/gofrs/uuid"
@@ -10,23 +11,39 @@ type Room struct {
 	ID        string    `gorm:"primaryKey" json:"id"`
 	Name      string    `json:"name"`
 	Turn      uint      `json:"turn"`
+	JoinURL   string    `json:"join_url"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func New(name string) (*Room, error) {
-	room := Room{Name: name}
-
 	id, err := uuid.NewV4()
 	if err != nil {
-		return &room, err
+		return &Room{}, err
 	}
-	room.ID = id.String()
-	room.Turn = 1
 
-	return &room, nil
+	return &Room{
+		ID:      id.String(),
+		Name:    name,
+		Turn:    1,
+		JoinURL: generateJoinURL(6),
+	}, nil
 }
 
 func (r *Room) IncrementTurn() {
 	r.Turn += 1
+}
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+func generateJoinURL(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
 }
